4c37b47eb86604ed949cbf5a9ea480914322e9af
alexander <alexandernyaga1999@gmail.com>
2023-10-20 15:33:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function createEmployeeRecord(employeeDataArray) {
+    const employeeRecord = {};
+
+    if(employeeDataArray.length > 0) {
+        employeeRecord.firstName = employeeDataArray[0];
+
+
+    if(employeeDataArray.length > 1) {
+        employeeRecord.familyName = employeeDataArray[1];
+
+        if(
+            employeeDataArray.length > 2) {
+                employeeRecord.title = employeeDataArray[2];
+
+                if(
+                    employeeDataArray.length > 3) {
+                        employeeRecord.payPerHour = employeeDataArray[3];
+            }      
+    }
+    return employeeRecord;
+
+}
+}
+}
